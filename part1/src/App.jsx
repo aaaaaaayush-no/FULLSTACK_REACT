e59b328ad26214a0d09d761ed3f8836f6274be8e
aaaaaaayush-no <081bei002.aayush@pcampus.edu.np>
2026-01-10 18:45:35 +0000

    test

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,54 +1,41 @@
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
+import { useState } from 'react'
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exercise}</p>
-    </div>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
+const Display = ({ counter }) => <div>{counter}</div>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
   }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Part part={part1.name} exercise={part1.exercises} />
-      <Part part={part2.name} exercise={part2.exercises} />
-      <Part part={part3.name} exercise={part3.exercises} />
-      <Hello name="Matti Luukkainen" age={21} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
-
-}
+} 
 
 export default App
